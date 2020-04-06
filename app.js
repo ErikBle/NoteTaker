31db46c6f3c16746071989cc3ee5bfb63fcc8a80
Yfopla <ekblecker@gmail.com>
2020-04-06 16:37:57 -0400

    Added: readfile to db.json, Added: Test text to the db.json file, Console log reads file

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ const PORT = process.env.PORT || 8000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+let dbNotes = []
+
 // When user starts, or when no page is found, defaults to the index file
-app.get("*", function(req, res) {
+app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
@@ -16,6 +18,16 @@ app.get("*", function(req, res) {
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
+
+// Reads the db.json file
+fs.readFile('./db/db.json', (err, data) => {
+    if (err) throw err;
+    let dbNotes = JSON.parse(data);
+    console.log(dbNotes)
+    
+});
+
+
 // Starts the server
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
